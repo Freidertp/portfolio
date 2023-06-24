c52dea30eaf501d801f1f95543cc70ee6e4e1c9e
Freidertp <freidertp@gmail.com>
2023-06-24 01:26:44 -0500

    updating

--- js/script.js
+++ js/script.js
@@ -1,1 +1,9 @@
-/*Not ready*/ 
+const container = document.querySelector('.container');
+const menuIcon = document.querySelector('.menu-icons');
+const containerNoScroll = document.querySelector('.container-noscroll');
+
+menuIcon.addEventListener('click', () => {
+   container.classList.replace('container', 'container-noscroll');
+   containerNoScroll.style.display = 'block';
+
+});
