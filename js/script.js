4daced1c74f54cfe522d606a9e3374268d9b3107
Freidertp <freidertp@gmail.com>
2023-06-28 20:26:06 -0500

    Mobile version completed.

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+const body = document.querySelector('body');
 const container = document.querySelector('.container')
 const navLink = document.querySelector('.nav__link')
 const menuIcon = document.querySelector('.menu-icons__icon')
@@ -13,17 +14,17 @@ const toggleMenu = () => {
 
    const isMobile = window.innerWidth < 700;
    const isContainerHidden = container.getAttribute('aria-hidden') === 'true';
-
+   body.style.overflow = isMobile && isContainerHidden ? 'hidden' : 'auto';
    menu.style.display = isMobile && isContainerHidden ? 'flex' : 'none';
    navLink.style.color = isMobile && isContainerHidden ? '#4831d4' : '#ccf381';
    menuIcon.classList.replace(
       isMobile && isContainerHidden ? 'menu-icons__icon' : 'menu-icons__icon--newcolor',
       isMobile && isContainerHidden ? 'menu-icons__icon--newcolor' : 'menu-icons__icon'
    );
-
+   section.style.display = isMobile && isContainerHidden ? 'none' : 'flex';
    figure.style.display = isMobile && isContainerHidden ? 'none' : 'block';
    image.style.display = isMobile && isContainerHidden ? 'none' : 'block';
-   section.style.display = isMobile && isContainerHidden ? 'none' : 'flex';
+
 
 };
 
